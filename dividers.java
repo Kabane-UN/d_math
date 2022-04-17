41f1d5d0b2fe96608a0b34f2fd927f0e8b56a327
Kabane-UN <60570949+Kabane-UN@users.noreply.github.com>
2022-04-17 16:20:14 +0300

    Create  dividers.java

---
+++ dividers.java
@@ -0,0 +1,41 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        List<Integer> dir = new ArrayList<Integer>();
+        for (int i = 1; i<= num; i++){
+            if(num%i == 0){
+                dir.add(i);
+            }
+        }
+        List<String> res = new ArrayList<>();
+        for (int i = dir.size() - 1; i >= 0; i--){
+            res.add(dir.get(i).toString()+';');
+            List<Integer> g = new ArrayList<>();
+            for (int j = i-1; j >= 0;j--){
+                if (dir.get(i) % dir.get(j) == 0){
+                    boolean flag = true;
+                    for (int k : g){
+                        if (k%dir.get(j) == 0){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag) {
+                        res.add(dir.get(i).toString() + "--" + dir.get(j).toString() + ';');
+                        g.add(dir.get(j));
+                    }
+                }
+            }
+        }
+        for(String s : res){
+            System.out.println(s);
+        }
+    }
+}
