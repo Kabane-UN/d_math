87003765b8d48205fb8862a9303891a6c09fe87d
Kabane-UN <60570949+Kabane-UN@users.noreply.github.com>
2022-05-03 14:37:50 +0300

    Update and rename mars.java to Mars.java

--- Mars.java
+++ Mars.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class mars {
+public class Mars {
     public static boolean compare(List<Integer> a, List<Integer> b) {
         int ismin = 0;
         for (int j = 0; j < Math.min(a.size(), b.size());  j++){
