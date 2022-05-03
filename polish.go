438dd96a0486c0205848c2a4f0da2dafa7ebcd92
Kabane-UN <60570949+Kabane-UN@users.noreply.github.com>
2022-05-03 11:19:14 +0300

    Update polish.go

--- polish.go
+++ polish.go
@@ -10,21 +10,34 @@ import (
 	"unicode"
 )
 
-func i_hate_parsing(s []string, index int) (*list.List, int) {
+func i_hate_parsing(s []string, index, countb int) (*list.List, int, int) {
 	parsed := list.New()
 	var c *list.List
 	if index >= len(s) {
-		return nil, index
+		return nil, index, countb
 	}
 	if s[index] == "(" {
+		k := countb
+		countb++
 		index++
-		c, index = i_hate_parsing(s, index)
+		c, index, countb = i_hate_parsing(s, index, countb)
 		parsed.PushBackList(c)
-		return parsed, index
+		for countb != k {
+			c, index, countb = i_hate_parsing(s, index, countb)
+
+			if c != nil {
+				if c.Front().Value == "*" || c.Front().Value == "+" || c.Front().Value == "-" {
+					parsed.PushBack(c)
+				} else {
+					parsed.PushBackList(c)
+				}
+			}
+		}
+		return parsed, index, countb
 	} else if s[index] != ")" {
 		parsed.PushBack(s[index])
 		index++
-		c, index = i_hate_parsing(s, index)
+		c, index, countb = i_hate_parsing(s, index, countb)
 		if c != nil {
 			if c.Front().Value == "*" || c.Front().Value == "+" || c.Front().Value == "-" {
 				parsed.PushBack(c)
@@ -32,13 +45,14 @@ func i_hate_parsing(s []string, index int) (*list.List, int) {
 				parsed.PushBackList(c)
 			}
 		} else {
-			return parsed, index
+			return parsed, index, countb
 		}
 	} else {
+		countb--
 		index++
-		return nil, index
+		return nil, index, countb
 	}
-	return parsed, index
+	return parsed, index, countb
 }
 func cal(tree *list.List) int {
 	var res int
